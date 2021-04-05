c6bdb9f86e9e379a4c5e310b606c2c662291d542
손주혜 <juanjh60@khu.ac.kr>
2021-04-05 20:53:46 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,8 @@ urlpatterns=[
     # 8000/ 주소로 들어왔을 때 views.post_list를 보여주라 말해줌
     # name='post_list'로 URL에 이름 붙인 것으로 뷰를 식별
     path('', views.post_list, name='post_list'),
+    # post/ : url이 post문자를 포함해야한다
+    # <int:pk> : 장고는 int값을 기대하고 이를 pk라는 변수로 뷰로 전송
+    # / : 다음에 /가 한번 더 와야 함
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 # 애플리케이션의 logic을 넣는 곳
 # 모델에서 필요한 정보를 받아와 템플릿에 전달
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 # models.py에 정의된 모델을 가져올 것
 from .models import Post
+
 
 
 # Create your views here.
@@ -12,3 +13,9 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     # posts를 템플릿에 넘겨줌
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+# 새로운 뷰 추가
+#
+def post_detail(request, pk):
+    post=get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
